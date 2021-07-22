4462dc63d6111efa812c04e62e619eec0fe76195
Utsab-crompoide <Khatiwodautsab22@gmail.com>
2021-07-22 14:21:49 +0545

    First commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCproject.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)   
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
